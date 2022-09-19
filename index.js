54e3e033d02b15549a7be49e914db424222b1f8a
Gabriel Barreto <gsbarretobr@gmail.com>
2022-09-18 21:39:00 -0300

    feat: added folder approach

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const { cutFiles } = require("./src/services/cutImages");
 
 const worker = createWorker({});
 
-const getContentFromImage = async (worker, filename) => {
-  const file = await fs.readFileSync(`./to-process/${filename}`);
-  const [name] = filename.split(".png");
-  console.log("Processing file>>>", name);
+const getContentFromImages = async (worker, folder) => {
+  const filesToProcess = await fs.readdirSync(`./to-process/${folder}`);
 
-  let {
-    data: { text },
-  } = await worker.recognize(file);
+  let textToSave = "";
 
-  await fs.writeFileSync(`./to-parse/${name}.txt`, text);
+  for (let i = 0; i < filesToProcess.length; i++) {
+    const filename = filesToProcess[i];
+    const file = await fs.readFileSync(`./to-process/${folder}/${filename}`);
+    const [name] = filename.split(".png");
+    console.log("Processing file>>>", name);
+    let {
+      data: { text },
+    } = await worker.recognize(file);
+    textToSave = `${textToSave}\n${text}`;
+  }
+
+  await fs.writeFileSync(`./to-parse/${folder}.txt`, textToSave);
 };
 
 (async () => {
@@ -24,10 +31,11 @@ const getContentFromImage = async (worker, filename) => {
   await worker.initialize("eng");
 
   console.log("## Starting image processing ");
-  const filesToProcess = await fs.readdirSync("./to-process");
+  const foldersToProcess = await fs.readdirSync("./to-process");
 
-  for (let i = 0; i < filesToProcess.length; i++) {
-    await getContentFromImage(worker, filesToProcess[i]);
+  for (let i = 0; i < foldersToProcess.length; i++) {
+    const folder = foldersToProcess[i];
+    await getContentFromImages(worker, folder);
   }
 
   await worker.terminate();
--- src/services/cutImages.js
+++ src/services/cutImages.js
@@ -3,15 +3,29 @@ const fs = require("fs");
 
 const cutFiles = async () => {
   console.log("## Started to cut images");
-  const filesToCut = await fs.readdirSync("./to-cut");
-  console.log("Images quantity >>>", filesToCut.length);
+  const dirToRead = await fs.readdirSync("./to-cut");
+  console.log("Cars quantity >>>", dirToRead.length);
 
-  for (let i = 0; i < filesToCut.length; i++) {
-    await sharp(`./to-cut/${filesToCut[i]}`)
-      // .extract({ left: 2757, top: 177, width: 1023, height: 1425 }) -- 4k
-      .extract({ left: 1413, top: 90, width: 507, height: 703 })
-      .toFile(`./to-process/${filesToCut[i]}`);
+  for (let i = 0; i < dirToRead.length; i++) {
+    const readingDirectory = dirToRead[i];
+    const filesToCut = await fs.readdirSync(`./to-cut/${readingDirectory}`);
+    console.log("Processing folder >>>", readingDirectory);
+    for (let j = 0; j < filesToCut.length; j++) {
+      const directoryExists = await fs.existsSync(
+        `./to-process/${readingDirectory}`
+      );
+
+      if (!directoryExists) {
+        await fs.mkdirSync(`./to-process/${readingDirectory}`);
+      }
+
+      await sharp(`./to-cut/${readingDirectory}/${filesToCut[j]}`)
+        // .extract({ left: 2757, top: 177, width: 1023, height: 1425 }) -- 4k
+        .extract({ left: 1413, top: 90, width: 507, height: 703 })
+        .toFile(`./to-process/${readingDirectory}/${filesToCut[j]}`);
+    }
   }
+
   console.log("## Finished to cut images");
 };
 
